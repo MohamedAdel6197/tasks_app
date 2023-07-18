b8dd674b305d1d4687ce3f2290b3ae4261c6fff9
mohamedadel6197 <mohamedadel6197@gmail.com>
2023-07-18 14:46:19 +0300

    make bottomNavigationBar and this Screens

--- lib/layouts/home_screen.dart
+++ lib/layouts/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tasks_app/modules/archived_tasks.dart';
+import 'package:tasks_app/modules/done_tasks.dart';
+import 'package:tasks_app/modules/tasks_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,12 +11,41 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<String> title = ["Tasks", "Done Tasks", "Archived Tasks"];
+  List<Widget> screens = [
+    const Tasks(),
+    const DoneTasks(),
+    const ArchivedTasks()
+  ];
+  int currentScreen = 0;
+  IconData icon = Icons.edit;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("ss"),
+        title: Text(title[currentScreen]),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(icon),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.menu), label: "Tasks"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.check_circle), label: "Done Tasks"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.archive_outlined), label: "Archived Tasks"),
+        ],
+        currentIndex: currentScreen,
+        type: BottomNavigationBarType.fixed,
+        onTap: (value) {
+          setState(() {
+            currentScreen = value;
+          });
+        },
+      ),
+      body: screens[currentScreen],
     );
   }
 }
